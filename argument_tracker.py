1f2fa768163f10a16c76ae44ee597c52fb39cd77
hac425 <hac425xxx@gmail.com>
2021-01-24 03:58:22 -0800

    merge define_task.py with argument_tracker.py

--- argument_tracker.py
+++ argument_tracker.py
@@ -453,8 +453,62 @@ def extract_ke_task_create(line):
     # print arg
     return arg.strip()
 
+def define_func(addr, name=""):
+    
+    if addr & 1:
+        addr -= 1
+        idaapi.split_sreg_range(addr, idaapi.str2reg("T"), 1, idaapi.SR_user)
+    else:
+        idaapi.split_sreg_range(addr, idaapi.str2reg("T"), 0, idaapi.SR_user)
+    
+    if idaapi.create_insn(addr):
+        idc.add_func(addr)
+        if name != "":
+            idaapi.set_name(addr, name,idaapi.SN_FORCE)
+
+def define_ke_state_handler(ea):
+    # undefined addr and define struct
+    sid = idaapi.get_struc_id("ke_state_handler")
+    struct_size = idaapi.get_struc_size(sid)
+    idaapi.del_items(ea, struct_size, idaapi.DELIT_DELNAMES)
+    idaapi.create_struct(ea, struct_size, sid)
+    
+    
+    msg_table = idaapi.get_dword(ea)
+    msg_cnt = idaapi.get_word(ea + 4)
+    
+    print "msg_table: 0x{:X}, msg_cnt: {}".format(msg_table, msg_cnt)
+    
+    sid = idaapi.get_struc_id("ke_msg_handler")
+    struct_size = idaapi.get_struc_size(sid)
+    idaapi.del_items(msg_table, struct_size, idaapi.DELIT_DELNAMES)
+    idaapi.create_struct(msg_table, struct_size, sid)
+    
+    idc.make_array(msg_table, msg_cnt)
+    
+    
+    for i in range(msg_cnt):
+        define_func(idaapi.get_dword(msg_table + 4 + i * 8))
+    
+    
+    
+def define_ke_task_desc(ea, task_desc_name):
+
+    print "{}: 0x{:X}".format(task_desc_name, ea)
+
+    sid = idaapi.get_struc_id("ke_task_desc")
+    struct_size = idaapi.get_struc_size(sid)
+    
+    # undefined addr and define struct
+    idaapi.del_items(ea, struct_size, idaapi.DELIT_DELNAMES)
+    idaapi.create_struct(ea, struct_size, sid)
+    idaapi.set_name(ea, task_desc_name, idaapi.SN_FORCE)
+
 
 def dump_ke_task_create():
+
+    retsult = {}
+
     logger = CustomLogger()
     m = CodeEmulator()
     at = ArgumentTracker()
@@ -470,10 +524,21 @@ def dump_ke_task_create():
                 reg = m.mu.reg_read(UC_ARM_REG_R1)
                 logger.log("addr: 0x{:X}, task_struct: 0x{:X}".format(xref.frm, reg))
 
+                retsult[xref.frm] = reg
 
-        logger.log("[decompile_tracer] addr: 0x{:X}, task_struct: 0x{:X}".format(xref.frm, at.decompile_tracer(xref.frm, extract_ke_task_create)[0]))
 
+        # logger.log("[decompile_tracer] addr: 0x{:X}, task_struct: 0x{:X}".format(xref.frm, at.decompile_tracer(xref.frm, extract_ke_task_create)[0]))
 
+    for k, v in retsult.items():
+        frm_func = idc.get_func_name(k)
+        task_desc_ea = v
+        task_desc_name = "{}_task_desc".format(frm_func.split("_init")[0])
+        define_ke_task_desc(task_desc_ea, task_desc_name)
+        
+        handler = idaapi.get_dword(task_desc_ea + 4)
+        define_ke_state_handler(handler)
+
+    return retsult
 
 def extract_ke_msg_alloc_msgid(line):
     target = re.findall("ke_msg_alloc\((.*?),", line)
@@ -530,6 +595,6 @@ def dump_msg_id_usage():
 
 
 if __name__ == "__main__":
-    # dump_ke_task_create()
-    dump_msg_id_usage()
+    dump_ke_task_create()
+    # dump_msg_id_usage()
 
