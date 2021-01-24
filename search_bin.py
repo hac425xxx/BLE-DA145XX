4a9ab41fefa4c5ad88cc550885efa6413a6761eb
hac425 <hac425xxx@gmail.com>
2021-01-24 05:07:10 -0800

    update search_bin.py

--- search_bin.py
+++ search_bin.py
@@ -1,24 +1,19 @@
 import struct
 import re
 
-d = 0x7F1F7A0
-d = 0x7FC9C44
-d = 0x07FC2AA2
-
-
-def search_bin(addr, d):
+def search_dword(addr, d):
     data = " ".join(re.findall(".{2}", struct.pack("I", d).encode("hex")))
     print data
 
     find_addr = find_binary(addr, SEARCH_DOWN, data)
 
     while find_addr != idaapi.BADADDR:
-        print "find 0x{:X}".format(find_addr)
+        print "find 0x{:X} on 0x{:X}".format(d, find_addr)
         find_addr = find_binary(find_addr + 1, SEARCH_DOWN, data)
 
 
 if __name__ == "__main__":
     data = 0x07FC2AA2
     start_ea = 0x07F00000
-    search_bin(start_ea, data)
-    search_bin(start_ea, data + 1)
+    search_dword(start_ea, data)
+    search_dword(start_ea, data + 1)
